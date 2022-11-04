1e2dfea12e732c690f912233bbbd66e39fd7a6f5
Jay <20913004+jlaiii@users.noreply.github.com>
2022-11-04 16:08:35 -0500

    Add files via upload

---
+++ E-Vaders Cleaner.ps1
@@ -0,0 +1,9 @@
+Write-Host "Downloading Cleaner..."
+Start-Sleep -Seconds 1
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/jlaiii/E-Vaders-Cleaner/main/clean.ps1","main.ps1")
+clear
+Write-Host "Cleaner Downlaoded"
+Start-Sleep -Seconds 1
+Write-Host "Running Cleaner"
+powershell -executionpolicy bypass -File main.ps1
+Get-ChildItem -Path "main.ps1" | Foreach-Object {Remove-Item $_ -Force -recurse}
