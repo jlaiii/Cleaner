7253427327af28c85d5bdb13850c2b2d304588a7
Jay <20913004+jlaiii@users.noreply.github.com>
2022-11-06 13:37:40 -0600

    Add files via upload

--- clean.ps1
+++ clean.ps1
@@ -1,7 +1,23 @@
 $ErrorActionPreference = 'SilentlyContinue'
-Start-Process powershell -Verb runAs
+param([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+
+'running with full privileges'
 Set-ExecutionPolicy "Unrestricted"
-$Title = "E-Vaders Cleaner v0.7"
+$Title = "E-Vaders Cleaner v0.8"
 $host.UI.RawUI.WindowTitle = $Title
 
 Get-ChildItem -Path "main.ps1" | Foreach-Object {Remove-Item $_ -Force -recurse}
