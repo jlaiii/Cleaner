1c5b10bc203da3cb12847c4968eaa27cae65a2d4
Jay <20913004+jlaiii@users.noreply.github.com>
2022-11-06 14:08:00 -0600

    Add files via upload

--- clean.ps1
+++ clean.ps1
@@ -1,6 +1,22 @@
 $ErrorActionPreference = 'SilentlyContinue'
-$Title = "E-Vaders Cleaner v2"
+$Title = "E-Vaders Cleaner v1.0.1"
 write-host "MUST RUN AS ADMIN!"
+
+ param([switch]$Elevated)
+function Check-Admin {
+$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+if ((Check-Admin) -eq $false)  {
+if ($elevated)
+{
+# could not elevate, quit
+}
+else {
+Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+}
+exit
+}
 
 
 clear
