96d9f9a1dc7c7dee027e0db70eb4f1e24c368b76
Jay <20913004+jlaiii@users.noreply.github.com>
2022-11-06 14:11:48 -0600

    Add files via upload

--- clean.ps1
+++ clean.ps1
@@ -3,22 +3,6 @@ $Title = "E-Vaders Cleaner v1.0.0"
 $host.UI.RawUI.WindowTitle = $Title
 write-host "MUST RUN AS ADMIN!"
 
- param([switch]$Elevated)
-function Check-Admin {
-$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-$currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
-if ((Check-Admin) -eq $false)  {
-if ($elevated)
-{
-# could not elevate, quit
-}
-else {
-Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-}
-exit
-}
-
 
 clear
 
