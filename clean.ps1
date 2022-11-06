0763d7f3ab2e5c5abf59def784675265603d143c
Jay <20913004+jlaiii@users.noreply.github.com>
2022-11-06 12:44:34 -0600

    Add files via upload

--- clean.ps1
+++ clean.ps1
@@ -42,7 +42,7 @@ Write-Host "Deleting Cheat Configs: LostCheats, PlantHack"
 Write-Host "Please WAIT!"
 
 $path0 = 'C:\'
-Get-ChildItem $path0 -Recurse -Force -Directory -Include 'EasyAnti*', 'BattlEye*', 'Riot Game*', | Remove-Item -Recurse -Confirm:$false -Force -Name
+Get-ChildItem $path0 -Recurse -Force -Directory -Include 'EasyAnti*', 'BattlEye*', 'Riot Game*' | Remove-Item -Recurse -Confirm:$false -Force -Name
 
 Remove-Item 'C:\Assets' -Recurse
 Remove-Item 'C:\Program Files (x86)\Rockstar Games' -Recurse
