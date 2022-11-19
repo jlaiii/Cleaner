c502490c747da12f8c03262f92f3b736f9adc290
Jay <20913004+jlaiii@users.noreply.github.com>
2022-11-19 01:16:17 -0600

    Add files via upload

--- E-Vaders Cleaner.ps1
+++ E-Vaders Cleaner.ps1
@@ -1,13 +1,11 @@
 $ErrorActionPreference = 'SilentlyContinue'
-$Title = "E-Vaders Launcher"
+$Title = "E-Vaders Launcher123"
 $host.UI.RawUI.WindowTitle = $Title
 Set-ExecutionPolicy Unrestricted -Force
 Write-Host "Downloading Cleaner..."
 Start-Sleep -Seconds 1
 (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/jlaiii/E-Vaders-Cleaner/main/clean.ps1","main.ps1")
 Write-Host "Cleaner Downlaoded"
-Start-Sleep -Seconds 1
 Write-Host "Running Cleaner"
-Start-Sleep -Seconds 1
 powershell -executionpolicy bypass -File main.ps1
 Get-ChildItem -Path "main.ps1" | Foreach-Object {Remove-Item $_ -Force -recurse}
